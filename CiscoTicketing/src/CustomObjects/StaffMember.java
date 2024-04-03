11ae4963d80b033bd437cd0c6cd5ae0262e6bd59
s3376686 <s3376686@student.rmit.edu.au>
2024-04-03 21:58:16 +1100

    User Story 1

--- CiscoTicketing/src/CustomObjects/StaffMember.java
+++ CiscoTicketing/src/CustomObjects/StaffMember.java
@@ -1,5 +1,58 @@
 package CustomObjects;
 
 public class StaffMember {
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+    private String password;
 
+    public StaffMember(String email, String fullName, String phoneNumber, String password) {
+        this.email = email;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    // Getters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "StaffMember{" +
+                "email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
