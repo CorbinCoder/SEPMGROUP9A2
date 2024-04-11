c39838a38b5eff7b39aa76b7fa57396abc98e726
Timothy Gollowitsch <timothy@gollowitsch.net>
2024-04-11 13:23:22 +1000

    fix seeding technicians use AccountValidator on staffMemberLogin

--- CiscoTicketing/src/Main/TicketingSystem.java
+++ CiscoTicketing/src/Main/TicketingSystem.java
@@ -364,10 +364,7 @@ public class TicketingSystem {
 		System.out.print("Enter your password: ");
 		String password = new String(System.console().readPassword());
 
-		// Find the staff member with the matching email address
-		StaffMember staffMember = findStaffMemberByEmail(email);
-
-		if (staffMember != null && staffMember.getPassword().equals(password)) {
+		if (AccountValidator.validateLoginDetails(email, password)) {
 			System.out.println("Login successful!");
 			// TODO: Proceed with staff member actions or display staff member menu
 		} else {
@@ -430,36 +427,36 @@ public class TicketingSystem {
 	private void seedTechnicians() {
 		var technicians = new ArrayList<Technician>() {
 			{
-				new Technician(
+				add(new Technician(
 						"harry.styles@team.cisco.com",
 						"Harry Styles",
 						"0425363455",
 						"P:\"/|\"\\)1pgi*{tCe#0.",
-						Level.ONE);
-				new Technician(
+						Level.ONE));
+				add(new Technician(
 						"niall.horan@team.cisco.com",
 						"Niall Horan",
 						"0425411004",
 						"7O7|dYH4PbogT?_P&<Hp",
-						Level.ONE);
-				new Technician(
+						Level.ONE));
+				add(new Technician(
 						"liam.payne@team.cisco.com",
 						"Liam Payne",
 						"0425002266",
 						",dP\"_sUQ\\EklO(0s+37c",
-						Level.ONE);
-				new Technician(
+						Level.ONE));
+				add(new Technician(
 						"louis.tomlinson@team.cisco.com",
 						"Louis Tomlinson",
 						"0425789123",
 						"QyDgn2a{1r&5\"F}LIXX&",
-						Level.TWO);
-				new Technician(
+						Level.TWO));
+				add(new Technician(
 						"zayn.malik@team.cisco.com",
 						"Zayn Malik",
 						"0425121333",
 						"W'oA%m%QV#TtYlc*Q_9_",
-						Level.TWO);
+						Level.TWO));
 			}
 		};
 
